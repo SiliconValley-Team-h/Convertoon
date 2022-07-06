12e044c5ee73f79d76411be2b8efd724491cf18d
Yerin Kim <87409442+Yerineee@users.noreply.github.com>
2022-07-06 18:21:48 +0900

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>Team-h</h1>;
+}
+
+export default App;
---
+++ src/pages/save.js
@@ -0,0 +1,14 @@
+import { React, Fragment } from 'react';
+import Header from './components/common/Header.js';
+import State from './components/common/State.js';
+
+function save() {
+  return (
+    <Fragment>
+      <Header />
+      <State />
+    </Fragment>
+  );
+}
+
+export default save;
