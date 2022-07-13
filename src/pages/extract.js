613422134ddd1c7720814c8293db2908e1ec61f0
Yerin <yeerin99@gmail.com>
2022-07-13 17:13:13 +0900

    img-id 전송 구현 중

--- src/pages/extract.js
+++ src/pages/extract.js
@@ -1,4 +1,5 @@
 import { Fragment, useState, useRef } from 'react';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 import Header from '../components/common/Header';
 import State from '../components/common/State';
@@ -9,11 +10,13 @@ import styles from './Extract.module.css';
 
 function Extract() {
   const [img, setImg] = useState(''); /* 선택한 이미지 파일 */
+  const [id, setId] = useState(0);
   const [visible, setVisible] = useState(false); /* 미리보기 활성화 여부 */
   const [disable, setDisable] = useState(true); /* 추출 버튼 활성화 여부 */
   const [movedisable, setMoveDisable] = useState(true);
   const [btnVisible, setBtnVisible] = useState(true); /* 추출 버튼 표시 여부 */
   const imageInput = useRef();
+  const formData = new FormData();
 
   const encodeFileToBase64 = fileBlob => {
     const reader = new FileReader(); /* reader 생성 */
@@ -31,12 +34,15 @@ function Extract() {
     event.preventDefault();
     encodeFileToBase64(event.target.files[0]);
 
-    const formData = new FormData();
     formData.append('image', event.target.files[0]);
-    axios.post('http://127.0.0.1:8000/api/results/', formData);
+    axios.post('http://127.0.0.1:8000/api/results/', formData).then(response => {
+      setId(4);
+      console.log(id);
+    });
 
     setVisible(true); /* 미리보기 활성화 */
     setDisable(false); /* 추출 버튼 활성화 */
+    console.log(disable);
   };
 
   const onClickInput = event => {
@@ -50,19 +56,16 @@ function Extract() {
     setBtnVisible(false);
   };
 
-  const onClickMove = event => {
-    event.preventDefault();
-    window.location.href = '/translate';
-  };
-
   return (
     <Fragment>
       <Header />
       <State>
         <p className={styles.text}>추출된 텍스트</p>
-        <button className={styles.moveBtn} onClick={onClickMove} disabled={movedisable}>
-          번역하러가기
-        </button>
+        <Link to={`/translate`} state={{ id: { id } }}>
+          <button className={styles.moveBtn} disabled={movedisable}>
+            번역하러가기
+          </button>
+        </Link>
       </State>
       <main className={styles.container}>
         <div className={styles.viewBox}>
@@ -70,7 +73,7 @@ function Extract() {
             <ImgView visible={visible} image={img} />
             <div>
               <input type="file" id="input-file" style={{ display: 'none' }} onChange={onChange} ref={imageInput} />
-              <button className={styles.selectBtn} for="input-file" onClick={onClickInput}>
+              <button className={styles.selectBtn} htmlFor="input-file" onClick={onClickInput}>
                 파일 선택
               </button>
             </div>
