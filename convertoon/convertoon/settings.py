a854071ea6123c0bc763901ce0f97fd4656ae3d8
deafsloth <deafsloth@gmail.com>
2022-07-13 18:23:17 +0900

    merging text insert & translation

--- convertoon/convertoon/settings.py
+++ convertoon/convertoon/settings.py
@@ -41,9 +41,11 @@ INSTALLED_APPS = [
     'rest_framework',
     'textExtract',
     'imagekit',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -128,3 +130,34 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+
+CORS_ORIGIN_WHITELIST = (
+    'http://127.0.0.1:3000', 
+    'http://localhost:3000', 
+    'http://convertoon.shop', 
+    'http://52.78.219.226:3000', 
+    'http://convertoon.shop:3000')
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+
+CORS_ALLOW_HEADERS = (
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+)
--- convertoon/textExtract/views.py
+++ convertoon/textExtract/views.py
@@ -8,7 +8,8 @@ from django.utils.decorators import method_decorator
 import json
 from django.core import serializers
 from rest_framework.response import Response
-from django.core.files.uploadedfile import InMemoryUploadedFile
+import urllib.request
+import urllib.parse
 
 from .models import SrcImg,ExtractText,ResultImg
 from .serializers import SrcImgSerializer,ResultImgSerializer,ExtractTextSerializer
@@ -121,3 +122,52 @@ def getInsTextImg(reqeust, img_id):
     return JsonResponse(serializer.data)
 
 
+
+@method_decorator(csrf_exempt, name="dispatch")
+def api_papago(request,img_id):
+    if request.method == 'POST': 
+        firstText = ExtractText.objects.filter(src_img_id=img_id).first()
+        lastText = ExtractText.objects.filter(src_img_id=img_id).last()
+        trstext_lists = []
+        cnt = 0
+
+        for i in range(firstText.text_id,lastText.text_id+1):
+            client_id = "BrFK4uZx3EBSvR_PfNIW" # 개발자센터에서 발급받은 Client ID 값
+            client_secret = "1teZNlNgnu" # 개발자센터에서 발급받은 Client Secret 값
+
+            source = ExtractText.objects.get(text_id=i)
+            encText = urllib.parse.quote(source.src_text)
+            
+
+            data = "source=ko&target=en&text=" + encText
+            url = "https://openapi.naver.com/v1/papago/n2mt"
+            request = urllib.request.Request(url)
+            request.add_header("X-Naver-Client-Id",client_id)
+            request.add_header("X-Naver-Client-Secret",client_secret)
+            response = urllib.request.urlopen(request, data=data.encode("utf-8"))
+            rescode = response.getcode()
+            if(rescode==200):
+                response_body = response.read()
+                result_dict = json.loads(response_body.decode('utf-8'))
+                translatedText = result_dict["message"]["result"]["translatedText"]
+                trstext_lists.append(translatedText)
+                source.trs_text = translatedText
+                source.save()
+                cnt += 1
+            else:
+                return HttpResponse("error")
+        return JsonResponse({
+            'text_lists' : trstext_lists,
+            'count' : cnt,
+            'img_id' : img_id,
+            }, json_dumps_params = {'ensure_ascii': True})
+
+def index(request,img_id):
+    ExtractText_list = ExtractText.objects.filter(src_img_id=img_id)
+    context = {'ExtractText_list': ExtractText_list}
+    return render(request,'textExtract/srcText_list.html',context)
+
+def trs_text_modify(request,img_id):
+    ExtractText_list = ExtractText.objects.filter(src_img_id=1)
+    context = {'ExtractText_list': ExtractText_list}
+    return render(request,'textExtract/srcText_list.html',context)
