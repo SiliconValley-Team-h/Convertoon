762a4d0e928b5addcb6e69bcf938c17a7ac3a45e
narlo23 <aks152312@gmail.com>
2022-07-13 17:07:58 +0900

    translate 원본 텍스트 서버에서 불러오기

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  /*<React.StrictMode>    얘때문에 useEffect가 두번 실행*/
+  <App />,
+  /*</React.StrictMode>,  */
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/pages/translate.js
+++ src/pages/translate.js
@@ -1,10 +1,26 @@
-import { React, Fragment } from 'react';
+import { React, Fragment, useEffect } from 'react';
 import styles from './Translate.module.css';
 import SelectLang from '../components/translate/SelectLang';
 import Buttons from '../components/translate/Buttons';
 import Header from '../components/common/Header';
 import TranslateField from '../components/translate/TranslateField';
-function Translate() {
+import { useLocation } from 'react-router-dom';
+import axios from 'axios';
+
+function Translate(location) {
+  /*const data = location.state.data;*/
+  useEffect(() => {
+    const headers = {
+      'Content-Type': 'application/json',
+    };
+    const url = 'http://127.0.0.1:8000/api/extractTexts/3'; /*url은 extract 페이지에서 link to로 받아와서 수정*/
+    /* const url = `http://127.0.0.1:8000/api/extractTexts/${data}`; */
+    axios.get(url, { headers }).then(function (response) {
+      // response
+      alert(JSON.stringify(response.data));
+    });
+  }, []);
+
   return (
     <Fragment>
       <Header />
