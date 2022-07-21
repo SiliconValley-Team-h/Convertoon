d3ca134383ed7ca0b80db3786b1db25924acd729
Yerin <yeerin99@gmail.com>
2022-07-21 14:59:11 +0900

    api 수정

--- src/services/API_Service.js
+++ src/services/API_Service.js
@@ -2,38 +2,34 @@ import axios from 'axios';
 
 const API_BASE_URL = 'http://127.0.0.1:8000/api';
 
-class API_Service {
-  setImg(img) {
-    return axios.post(API_BASE_URL + '/results/', img);
-  }
-
-  getAllText(img_id) {
-    return axios.get(API_BASE_URL + `/extractTexts/${img_id}/`);
-  }
-
-  getSrcImg(img_id) {
-    return axios.get(API_BASE_URL + `/getSrcImg/${img_id}/`);
-  }
-
-  getTransText(img_id) {
-    return axios.post(API_BASE_URL + `/translate/${img_id}/`);
-  }
-
-  getResultImg(img_id) {
-    return axios.get(API_BASE_URL + `getInsTextImg/${img_id}/`);
-  }
-
-  setSrcText(img_id) {
-    return axios.post(API_BASE_URL + `srcModify/${img_id}/`);
-  }
-
-  setTrsText(img_id) {
-    return axios.post(API_BASE_URL + `trsModify/${img_id}/`);
-  }
-
-  setLang(img_id) {
-    return axios.post(API_BASE_URL + `translate/${img_id}/`);
-  }
+export function getImgId(img) {
+  return axios.post(API_BASE_URL + '/results/', img);
 }
 
-export default new API_Service();
+export function getAllText(img_id) {
+  return axios.get(API_BASE_URL + `/extractTexts/${img_id}/`);
+}
+
+export function getSrcImg(img_id) {
+  return axios.get(API_BASE_URL + `/getSrcImg/${img_id}/`);
+}
+
+export function getTransText(img_id) {
+  return axios.post(API_BASE_URL + `/translate/${img_id}/`);
+}
+
+export function getResultImg(img_id) {
+  return axios.get(API_BASE_URL + `getInsTextImg/${img_id}/`);
+}
+
+export function setSrcText(img_id) {
+  return axios.post(API_BASE_URL + `srcModify/${img_id}/`);
+}
+
+export function setTrsText(img_id, trsModTexts, cnt) {
+  return axios.post(API_BASE_URL + `trsModify/${img_id}/`, { text_lists: trsModTexts, count: cnt, img_id: img_id });
+}
+
+export function setLang(img_id, srcModTexts, cnt) {
+  return axios.post(API_BASE_URL + `translate/${img_id}/`, { text_lists: srcModTexts, count: cnt, img_id: img_id });
+}
